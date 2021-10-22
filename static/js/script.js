b7fd6ee63eca4b0e54f7387091e6e3b2a089c6a5
George <glychock@gmail.com>
2021-10-22 09:57:55 -0400

    feature: Finished Add Ingredient section on the Build Recipe page

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     $(".tooltipped").tooltip();
 });
 
+function clearLocalStorage(lsName) {
+    localStorage.clear(lsName);
+}
+
 function ratingConfirm() {
     let txt = "Thank you for submitting your rating!";
     confirm(txt)
@@ -14,21 +18,36 @@ function addIngredient() {
         measure: "",
         id: ""
     };
-        //Add a unique-ish ingredient ID to the data
-        passIngredientData.id = "ing" + Math.floor(Math.random()*10000000);
-        //grab input form data from modal
-        passIngredientData.name = document.getElementById("ingredientForm").elements.namedItem("ingredient_region_1_name").value;
-        passIngredientData.quantity = document.getElementById("ingredientForm").elements.namedItem("ingredient_region_1_quantity").value;
-        passIngredientData.measure = document.getElementById("ingredientForm").elements.namedItem("ingredient_region_1_measure").value;
+    //Add a unique-ish ingredient ID to the data
+    passIngredientData.id = "ing" + Math.floor(Math.random()*10000000);
+    //grab input form data from modal
+    passIngredientData.name = document.getElementById("ingredientForm").elements.namedItem("ingredient_region_1_name").value;
+    passIngredientData.quantity = document.getElementById("ingredientForm").elements.namedItem("ingredient_region_1_quantity").value;
+    passIngredientData.measure = document.getElementById("ingredientForm").elements.namedItem("ingredient_region_1_measure").value;
+
+    // Add ingredient ID to localStorage
+    lsName = "ingredientIDLocalStorage";
+    addToLocalStorage(passIngredientData.id, lsName);
 
     return $("#ingredient_region").append(buildIngredientForm(passIngredientData));
+}
+
+function removeIngredient(iID) {
+    // Remove ingredient ID from localStorage and remove ingredient from user input form
+    console.log(iID);
+    lsName = "ingredientIDLocalStorage";
+    elementID = iID;
+    removeFromLocalStorage(elementID, lsName);
+    //remove panel
+    return $("#" + elementID).remove();
+
 }
 
 function buildIngredientForm(i) {
     let ing = i;
 
     return `
-    <div class="row">
+    <div class="row us-ingredient-row" id="${ing.id}">
         <div class="col-4 us-ingredient-name">
             <label for="ingredient_name" class="form-label">Ingredient Name</label>
             <input class="" type="text" id="ingredient_name" name="ingredient_name" value="${ing.name}">
@@ -43,52 +62,58 @@ function buildIngredientForm(i) {
         </div>
         <!-- Remove the ingredient from the form -->
         <div class="col-2">
-            <button type="button" class="us-ingredient-addbtn" onclick="removeIngredient(${ing.id})">+ Remove</button>
+            <button type="button" class="us-ingredient-addbtn" onclick="removeIngredient('${ing.id}')">Remove</button>
         </div>
     </div>
     `
 }
 
-function buildIngredientForm2(i) {
-    let inc = i;
-
-    return `
-    <div class="ingredient_region${inc}">
-        <label for="ingredient_region${inc}_name" class="form-label">Ingredient Name</label>
-        <input class="" id="ingredient_region${inc}_name" name="ingredient_region${inc}_name">
-        <label for="ingredient_region${inc}_measure" class="form-label">Measure</label>
-        <select id="ingredient_region${inc}_measure" name="ingredient_region${inc}_measure" class="form-select us-dropdown">
-            <option selected>Choose...</option>
-            {% for measure in measures %}
-            <option>{{ measure.measure_name }}</option>
-            {% endfor %}
-        </select>
-    </div>
-    `
-}
-
-function addIngredientStorage(iNum) {
-    var localStoreName = "ingredientNumber";
-    var ingredientNum = iNum;
+function addToLocalStorage(storeValue, lsName) {
+    var localStoreName = lsName;
+    var valueToSave = storeValue;
 
     // Check if localStorage is enabled
     if (storageAvailable('localStorage')) {
         // We can use localStorage, check if localStorage var has been initiated
         if (localStorage.getItem(localStoreName)) {
             // Add widget ID to the localStorage string and store
-            let ingredientNumToSave = localStorage.getItem(localStoreName).split(','); //returns a string, comma delimited, convert to array
-            ingredientNumToSave.push(ingredientNum);
-            localStorage.setItem(localStoreName, ingredientNumToSave); //stored as a string, comma delimited
+            let valuesSaved = localStorage.getItem(localStoreName).split(','); //returns a string, comma delimited, convert to array
+            valuesSaved.push(valueToSave);
+            localStorage.setItem(localStoreName, valuesSaved); //stored as a string, comma delimited
         } else {
             // Initiate localStorage and add project ID
-            let ingredientNumToSave = [];
-            ingredientNumToSave.push(ingredientNum);
-            localStorage.setItem(localStoreName, ingredientNumToSave);
+            let valuesSaved = [];
+            valuesSaved.push(valueToSave);
+            localStorage.setItem(localStoreName, valuesSaved);
+
+            valuesSaved = localStorage.getItem(localStoreName).split(',');
+            console.log("Add to LS" + valuesSaved);
         }
     } else {
         // no localStorage
         return alert("Your browser does not support localStorage use for this domain at this time. This will effect how your dashboard looks when you reopen The Project Management Dashboard in a new browser window.");
     }
+}
+
+function removeFromLocalStorage(storeValue, lsName) {
+
+    var localStoreName = lsName;
+    var valueToDel = storeValue;
+
+    console.log(localStoreName);
+    console.log(valueToDel);
+
+    if (localStorage.getItem(localStoreName)) {
+        let savedValues = localStorage.getItem(localStoreName).split(',');
+        let delValueIndex = savedValues.indexOf(valueToDel);
+        var removedWidgetIDs = savedValues.splice(delValueIndex, 1);
+
+        localStorage.setItem(localStoreName, savedValues);
+
+        valuesSaved = localStorage.getItem(localStoreName).split(',');
+        console.log("Remaining ingredients" + valuesSaved);
+
+    } // else capture error, if needed
 }
 
 /* Check to make sure localStorage is usuable in the browser */
